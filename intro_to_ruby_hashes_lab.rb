d9593010b54d1369d732ea985d8e93225adaa600
Charlie Russo <cparusso@gmail.com>
2018-08-03 20:49:54 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 		railroads: {
-			pieces: 4, 
+			pieces: 4,
 			names: {
 				reading_railroad: {},
 				pennsylvania_railroad: {},
@@ -38,7 +38,9 @@ end
 
 def monopoly_with_fourth_tier
 	monopoly = {
-		railroads: {pieces: 4, names: {
+		railroads: {
+			pieces: 4,
+			names: {
 				reading_railroad: {
 					mortgage_value: "$100"
 				},
@@ -50,8 +52,8 @@ def monopoly_with_fourth_tier
 				},
 				shortline: {
 					mortgage_value: "$800"
-				}
-			}, rent_in_dollars: {
+				}},
+			rent_in_dollars: {
 				one_piece_owned: 25,
 				two_pieces_owned: 50,
 				three_pieces_owned: 100,
